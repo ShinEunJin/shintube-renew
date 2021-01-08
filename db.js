84c23043c3ecf083e432e4f3471c9e0f4cc240b4
ShinEunJin <shineun5501@naver.com>
2021-01-08 16:30:06 +0900

    video part backend complete

--- db.js
+++ db.js
@@ -1,50 +1,13 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-        creator: {
-            id: 121212,
-            name: "Shin",
-            email: "shin@nav.com"
-        }
-    },
-    {
-        id: 12323,
-        title: "What the",
-        description: "This is something I love",
-        views: 3634,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-        creator: {
-            id: 1130412,
-            name: "Shin",
-            email: "shin@nav.com"
-        }
-    },
-    {
-        id: 11111,
-        title: "Video e",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-        creator: {
-            id: 777888,
-            name: "Shin",
-            email: "shin@nav.com"
-        }
-    },
-    {
-        id: 77777,
-        title: "awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-        creator: {
-            id: 123423423212,
-            name: "Shin",
-            email: "shin@nav.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
